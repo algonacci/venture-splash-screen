671a60213eab0ef230e3c067bf703e2af93deae3
algonacci <algonacci1@gmail.com>
2022-10-13 13:26:00 +0700

    Add navigation to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:venture/pages/home.dart';
 import 'package:venture/pages/splash.dart';
 
 void main() {
@@ -10,8 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+    return MaterialApp(
+      home: const SplashScreen(),
+      routes: {
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 5),
+      () => Navigator.popAndPushNamed(
+        context,
+        '/home',
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
